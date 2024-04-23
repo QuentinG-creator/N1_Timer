7e5367fb4de21ca46c71068b9c67defdc5396868
QuentinG-creator <73114129+QuentinG-creator@users.noreply.github.com>
2024-04-23 15:20:43 +0200

    Update

--- script.js
+++ script.js
@@ -1,20 +1,190 @@
 Office.onReady((info) => {
   console.log("Office.js is now ready in ${info.host} host.");
+  getDomaine();
+  $("#ListDomaine").select2({
+    placeholder: "Select an option",
+    width: "100%"
+  });
   $("#initialisation").on("click", () => tryCatch(initialisation));
+  $("#Start").on("click", () => tryCatch(start_Timer));
+  $("#Stop").on("click", () => tryCatch(stop_Timer));
+  $("#Pause").on("click", () => tryCatch(pause));
+  $("#Reprendre").on("click", () => tryCatch(reprendre));
 });
 
-let incidentTimer = {};
-
+var tab_timers=[]
+var timer = 0;
+var time_spend_pause = 0;
+var is_paused = false;
 // This function is for refresh the select when the number of incident is modify.
-function refreshList(ids) {
-  var select = document.getElementById("IdIncident");
+function pause() {
+  if (is_paused == false && timer != 0) {
+    var actualTime = new Date();
+    time_spend_pause = actualTime.getTime() - timer;
+    is_paused = true;
+  } else alert("Votre timer est déjà en pause ou alors vous n'avez pas démarrer de timer");
+}
 
-  select.innerHTML = "";
-  ids.forEach(function(option) {
-    var el = document.createElement("option");
-    el.textContent = option;
-    el.value = option;
-    select.appendChild(el);
+function reprendre() {
+  if (is_paused == true) {
+    var actualTime = new Date();
+    timer = actualTime.getTime() - time_spend_pause;
+    is_paused = false;
+  } else alert("Votre timer n'est pas en pause");
+}
+
+function displayTimer(tab_timers) {
+  // Fund the the table by the id
+  const table = document.getElementById('timersTable');
+
+  // Effacer les lignes existantes
+  table.innerHTML = '';
+
+  for (const [domain, timer] of Object.entries(tab_timers)) {
+    let row = table.insertRow();
+    let cellDomain = row.insertCell(0);
+    let cellStartTime = row.insertCell(1);
+
+    cellDomain.innerHTML = domain;
+    cellStartTime.innerHTML = timer;
+  }
+}
+
+function stop_Timer() {
+  const nniInput = document.getElementById("NNI");
+  const nniValue = nniInput.value;
+
+  const domaineInput = document.getElementById("ListDomaine");
+  const domaineValue = domaineInput.value;
+
+  var flag = 0;
+
+  if (!nniValue) {
+    alert("! Veuillez entrer votre NNI !");
+    return;
+  }
+
+  if (!timer) {
+    alert("! Le timer n'est pas lancé !");
+    return;
+  }
+
+  if (is_paused) {
+    alert("! Le timer est en pause !");
+    return;
+  }
+
+  return Excel.run(function(context) {
+    // DMT sheet **************************/
+    var sheetDMT = context.workbook.worksheets.getItem("DMT");
+
+    var usedRangesheetDMT = sheetDMT.getUsedRange();
+    usedRangesheetDMT.load("rowCount");
+
+    // DMT total sheet ********************/
+    var sheetDMTTtl = context.workbook.worksheets.getItem("DMT Total");
+
+    var usedRangeSheetDMTTtl = sheetDMTTtl.getUsedRange();
+    usedRangeSheetDMTTtl.load("rowCount");
+
+    // nb demande sheet ********************/
+    var sheetNbD = context.workbook.worksheets.getItem("Nb demande");
+
+    var usedRangeSheetNbD = sheetNbD.getUsedRange();
+    usedRangeSheetNbD.load("rowCount");
+
+    var headerRangeDMT = sheetDMT.getRange("A1:ZZ1");
+    headerRangeDMT.load("values");
+
+    var headerRangeDMTTtl = sheetDMTTtl.getRange("A1:ZZ1");
+    headerRangeDMTTtl.load("values");
+
+    var headerRangeNbD = sheetNbD.getRange("A1:ZZ1");
+    headerRangeNbD.load("values");
+
+    return context.sync().then(function() {
+      var lastRowDMT = usedRangesheetDMT.rowCount;
+      var actualDate = new Date().toLocaleDateString();
+
+      var columnIndex = headerRangeDMT.values[0].indexOf(domaineValue);
+      if (columnIndex === -1) {
+        throw new Error("L'entête spécifié n'a pas été trouvé.");
+      }
+
+      var rangeNNI = sheetDMT.getRange("B2:B" + lastRowDMT);
+      var rangeDate = sheetDMT.getRange("A2:A" + lastRowDMT);
+
+      rangeNNI.load("values");
+      rangeDate.load("values");
+
+      return context.sync().then(function() {
+        for (var i = 0; i < rangeDate.values.length; i++) {
+          if (rangeDate.values[i][0] === actualDate && rangeNNI.values[i][0] === nniValue) {
+            var goodRow = i + 1;
+            flag = 1;
+            break;
+          }
+        }
+
+        if (flag == 1) {
+          var idDomaineCellDMT = sheetDMT.getCell(goodRow, columnIndex);
+          idDomaineCellDMT.load("values");
+
+          var idDomaineCellDMTTtl = sheetDMTTtl.getCell(goodRow, columnIndex);
+          idDomaineCellDMTTtl.load("values");
+
+          var idDomaineCellNbD = sheetNbD.getCell(goodRow, columnIndex);
+          idDomaineCellNbD.load("values");
+          return context.sync().then(function() {
+            var actualTime = new Date();
+            idDomaineCellDMT.values = [[idDomaineCellDMT.values[0][0] + (actualTime.getTime() - timer) / 1000]];
+            idDomaineCellNbD.values = [[idDomaineCellNbD.values[0][0] + 1]];
+            idDomaineCellDMTTtl.values = [[idDomaineCellDMT.values[0][0] / idDomaineCellNbD.values[0][0]]];
+            timer = 0;
+            tab_timers[domaineValue] = idDomaineCellDMTTtl.values[0][0]
+            displayTimer(tab_timers)
+            return context.sync();
+          });
+        } else {
+          alert("! Veuillez appuyer sur initialiser avec votre NNI avant tout autre action !");
+          return context.sync();
+        }
+      });
+    });
+  });
+}
+
+function getDomaine() {
+  var select = document.getElementById("ListDomaine");
+
+  var domaines = {};
+
+  return Excel.run(function(context) {
+    // We get the sheet Save for doing operation on it.
+    var params = context.workbook.worksheets.getItem("Params");
+    var usedRange = params.getUsedRange(true);
+    usedRange.load("rowCount");
+
+    return context.sync().then(function() {
+      var lastRow = usedRange.rowCount;
+      var range = params.getRange("A2:A" + lastRow);
+      range.load("values"); // Charger les valeurs
+
+      return context.sync().then(function() {
+        var values = range.values;
+        for (var i = 0; i < values.length; i++) {
+          let key = values[i][0];
+          domaines[key] = 0;
+        }
+        select.innerHTML = "";
+        Object.keys(domaines).forEach(function(option) {
+          var el = document.createElement("option");
+          el.textContent = option;
+          el.value = option;
+          select.appendChild(el);
+        });
+      });
+    });
   });
 }
 
@@ -23,34 +193,111 @@ function initialisation() {
   const nniInput = document.getElementById("NNI");
   const nniValue = nniInput.value;
 
+  var flag = 0;
+
   if (nniValue) {
     return Excel.run(function(context) {
-      var nniSheet = context.workbook.worksheets.getItem(nniValue);
+      // sheetDMT initialisation ***************************************************/
+      var sheetDMT = context.workbook.worksheets.getItem("DMT");
 
       // Load all row for check where we are going to put the value
-      var usedRangeNniSheet = nniSheet.getUsedRange();
-      usedRangeNniSheet.load("rowCount");
+      var usedRangesheetDMT = sheetDMT.getUsedRange();
+      usedRangesheetDMT.load("rowCount");
+
+      // nb demande sheet initialisation ***************************************************/
+
+      var sheetNbD = context.workbook.worksheets.getItem("Nb demande");
+
+      // Load all row for check where we are going to put the value
+      var usedRangeSheetNbD = sheetNbD.getUsedRange();
+      usedRangeSheetNbD.load("rowCount");
+
+      // DMT Total initialisation ***************************************************/
+
+      var sheetDMTTtl = context.workbook.worksheets.getItem("DMT Total");
+
+      // Load all row for check where we are going to put the value
+      var usedRangeSheetDMTTtl = sheetDMTTtl.getUsedRange();
+      usedRangeSheetDMTTtl.load("rowCount");
 
       return context.sync().then(function() {
-        var lastRowNniSuivi = usedRangeNniSheet.rowCount; // The last row used in 'Suivi'
+        //DMT Total sheet range initialisation ***************************************************/
+
+        var lastRowDMT = usedRangeSheetDMTTtl.rowCount; // The last row used in 'Suivi'
+        var rangeDateDMT = sheetDMTTtl.getRange("A2:A" + lastRowDMT);
+        rangeDateDMT.load("values");
+        var rangeNNIDMT = sheetDMTTtl.getRange("B2:B" + lastRowDMT);
+        rangeNNIDMT.load("values");
+
+        // nb Demande range initialisation  ***************************************************/
+
+        var lastRowNbD = usedRangeSheetNbD.rowCount; // The last row used in 'Suivi'
+        var rangeDateNbD = sheetNbD.getRange("A2:A" + lastRowNbD);
+        rangeDateNbD.load("values");
+        var rangeNNINbD = sheetNbD.getRange("B2:B" + lastRowNbD);
+        rangeNNINbD.load("values");
+
+        // DMT range initialisation ***************************************************/
+
+        var lastRowNniSuivi = usedRangesheetDMT.rowCount; // The last row used in 'Suivi'
+        var rangeDate = sheetDMT.getRange("A2:A" + lastRowNniSuivi);
+        rangeDate.load("values");
+        var rangeNNI = sheetDMT.getRange("B2:B" + lastRowNniSuivi);
+        rangeNNI.load("values");
 
         return context.sync().then(function() {
-          var nniCellNni = nniSheet.getCell(lastRowNniSuivi, 1);
-          var idDateCell = nniSheet.getCell(lastRowNniSuivi, 0);
-          var actualDate = new Date()
-                                          
-          nniCellNni.values = [[nniValue]];
-          idDateCell.values = [[actualDate.toLocaleDateString()]];
+          // DMT cells
+          var nniCellNni = sheetDMT.getCell(lastRowNniSuivi, 1);
+          var idDateCell = sheetDMT.getCell(lastRowNniSuivi, 0);
+
+          // NbD cells
+          var nniCellNniNbD = sheetNbD.getCell(lastRowNbD, 1);
+          var idDateCellNbD = sheetNbD.getCell(lastRowNbD, 0);
+
+          // DMT Total cells
+          var nniCellNniDMT = sheetDMTTtl.getCell(lastRowDMT, 1);
+          var idDateCellDMT = sheetDMTTtl.getCell(lastRowDMT, 0);
+
+          var actualDate = new Date();
+          var valuesDate = rangeDate.values;
+          var valuesNNI = rangeNNI.values;
+          for (var i = 0; i < valuesDate.length; i++) {
+            if (valuesDate[i][0] === actualDate.toLocaleDateString() && valuesNNI[i][0] === nniValue) {
+              flag = 1;
+            }
+          }
+
+          if (flag == 0) {
+            nniCellNni.values = [[nniValue]];
+            idDateCell.values = [[actualDate.toLocaleDateString()]];
+
+            nniCellNniNbD.values = [[nniValue]];
+            idDateCellNbD.values = [[actualDate.toLocaleDateString()]];
+
+            nniCellNniDMT.values = [[nniValue]];
+            idDateCellDMT.values = [[actualDate.toLocaleDateString()]];
+
+            alert("Vous avez bien initialiser");
+          } else {
+            alert("! Vous avez déjà initialiser votre NNI pour aujourd'hui !");
+          }
 
           return context.sync();
         });
       });
     });
   } else {
-    console.log("Entrer le NNI et l'application concerné avant de vouloir ajouter un incident !");
+    alert("! Veuillez entrer votre NNI !");
   }
 }
 
+//Start the timer
+function start_Timer() {
+  if (timer == 0) {
+    var actualDate = new Date();
+    timer = actualDate.getTime();
+  } else alert("vous avez déjà un incident en cours");
+}
 /** Default helper for invoking an action and handling errors. */
 function tryCatch(callback) {
   Promise.resolve()
@@ -59,50 +306,4 @@ function tryCatch(callback) {
       // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
       console.error(error);
     });
-}
-
-// Set the past time between "prise en charge" and "sollicitation" in the correct cell
-function sollicitation() {
-  return Excel.run(function(context) {
-    var suivi = context.workbook.worksheets.getItem("Suivi");
-    var save = context.workbook.worksheets.getItem("Save");
-
-    const select = document.getElementById("IdIncident");
-    const id = select.value;
-
-    // Load all row from the worksheet
-    var usedRangeSuivi = suivi.getUsedRange();
-    usedRangeSuivi.load("rowCount");
-
-    // Load all row from the workseets save
-    var usedRangeSave = save.getUsedRange();
-    usedRangeSave.load("rowCount");
-
-    return context.sync().then(function() {
-      var searchCell = usedRangeSuivi.find(id, { matchCase: true });
-      searchCell.load("rowIndex");
-      var saveCell = usedRangeSave.find(id, { matchCase: true });
-      saveCell.load("rowIndex");
-      return context.sync().then(function() {
-        var sollicitationCell = suivi.getCell(searchCell.rowIndex, 2);
-        sollicitationCell.load("values");
-
-        var saveTimer = save.getCell(saveCell.rowIndex, 1);
-        saveTimer.load("values");
-
-        return context.sync().then(function() {
-          if (sollicitationCell.values[0][0] === null || sollicitationCell.values[0][0] === "") {
-            var actualTime = new Date();
-            sollicitationCell.values = [
-              [(actualTime.getTime() - incidentTimer[id]) / 1000 / 60 + saveTimer.values[0][0]]
-            ];
-          } else {
-            console.log("Vous avez déjà renseigner cette catégorie pour l'incident que vous avez selectionné");
-          }
-
-          return context.sync();
-        });
-      });
-    });
-  });
 }
